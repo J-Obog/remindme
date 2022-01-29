ba7c8809721883c6ecb5bdbc61487ff8bc9369eb
JObog <jobogbaimhe@gmail.com>
2022-01-29 15:55:27 -0500

    adding subpath imports for module aliasing

--- server/index.js
+++ server/index.js
@@ -1,11 +1,7 @@
 const express = require("express");
-const logger = require("./logger");
+const logger = require("#utils/logger");
 const app = express();
 const port = 8080;
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
-});
 
 app.listen(port, () => {
   logger.info("Hello world");
