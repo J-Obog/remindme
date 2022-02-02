c0f90979440be61650450325d24040618ae92a27
JObog <jobogbaimhe@gmail.com>
2022-02-01 22:43:03 -0500

    using dotenv for env variables and setting up db connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
+const dotenv = require("dotenv");
+
+if (process.env.NODE_ENV === "development") {
+  dotenv.config({ path: ".env.dev" });
+}
+
 const express = require("express");
 const logger = require("#utils/logger");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8089;
 
 app.listen(port, () => {
   logger.info(`Server running on port ${port}`);
