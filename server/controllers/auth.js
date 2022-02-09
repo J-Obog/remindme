c4e48ff852301d98513559a37004c47a73fa5a47
JObog <jobogbaimhe@gmail.com>
2022-02-08 21:32:02 -0500

    added check for email uniqueness in registration

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,31 +1,33 @@
 const User = require("#models/user");
 const { userRegSchema, formatJoiErrors } = require("#utils/validation");
+const bcrypt = require("bcrypt");
 
 exports.logUserIn = async (req, res) => {};
 
-module.exports.registerNewUser = async (req, res) => {
+exports.registerNewUser = async (req, res) => {
   const { firstName, lastName, email, password, birthDate } = req.body;
+  const { error } = userRegSchema.validate(req.body, { abortEarly: false });
+
+  if (error) return res.status(401).json({ message: formatJoiErrors(error) });
 
   try {
-    await userRegSchema.validateAsync(req.body, { abortEarly: false });
-  } catch (e) {
-    return res.status(401).json({
-      message: formatJoiErrors(e),
-    });
-  }
+    const user = await User.findOne({ where: { email: email } });
+    if (user)
+      return res
+        .status(401)
+        .json({ message: { email: "Account with email already exists" } });
 
-  try {
+    const phash = await bcrypt.hash(password, 10);
+
     await User.create({
       firstName: firstName,
       lastName: lastName,
       email: email,
-      password: password,
+      password: phash,
       birthDate: birthDate,
     });
 
-    return res.status(200).json({
-      message: "Account successfully created",
-    });
+    return res.status(200).json({ message: "Account successfully created" });
   } catch (e) {
     return res.status(500).json(e);
   }
