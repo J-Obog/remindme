6e79e787f4f9f0d9439d5315b22ceb2ead589cf9
JObog <jobogbaimhe@gmail.com>
2022-02-10 17:43:48 -0500

    adding prettier and eslint configs

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,9 @@
 import React from "react";
-import bg from "./assets/bg.svg";
 
 const App = () => {
   return (
-    <div
-      className="App p-1 flex flex-col items-center justify-center animate-bg-fade-in"
-      style={{
-        backgroundImage: `url(${bg})`,
-        backgroundPosition: "center",
-        backgroundAttachment: "fixed",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        height: "100vh",
-        weight: "100vw",
-        overflow: "auto",
-      }}
-    >
-      <div className="bg-white w-2/6 px-16 py-8 shadow-xl rounded-xl flex flex-col items-center justify-center">
-        <h1 class="text-3xl">Hello</h1>
-      </div>
+    <div className="App">
+      <h1>App</h1>
     </div>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      animation: {
-        "bg-fade-in": "bg-fade-in 1s ease-out",
-      },
-      keyframes: {
-        "bg-fade-in": { from: { opacity: 0 }, to: { opacity: 1 } },
-      },
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                kayak: ['"Kayak Sans"', 'sans-serif'],
+            },
+            animation: {
+                'bg-fade-in': 'bg-fade-in 1s ease-out',
+            },
+            keyframes: {
+                'bg-fade-in': { from: { opacity: 0 }, to: { opacity: 1 } },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
