9673c4327c7761d4c38d9dd9e4a7370c52430f21
JObog <jobogbaimhe@gmail.com>
2022-02-12 19:06:56 -0500

    aliasing styles

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,30 +3,30 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className="bg-white w-1/4 shadow-xl px-8 py-8 rounded-xl flex flex-col gap-16 items-center justify-start">
+        <div className="auth-form-container">
             <div>
                 {/* 
                 - Logo
                 - Spiel
             */}
-                <h1 className="font-bold text-2xl text-gray-600">Log In</h1>
+                <h1 className="auth-form-htext">Log In</h1>
             </div>
             <div className="flex flex-col items-center gap-10 w-full">
                 <div className="w-full">
-                    <input className="text-xl text-gray-800 border-b-2 border-gray-200 outline-none w-full" placeholder="Email" />
+                    <input className="auth-form-input" placeholder="Email" />
                 </div>
                 <div className="w-full">
-                    <input className="text-xl text-gray-800 border-b-2 border-gray-200 outline-none w-full" type="password" placeholder="Password" />
+                    <input className="auth-form-input" type="password" placeholder="Password" />
                 </div>
                 <div className="w-full">
-                    <button className="font-bold text-white text-xl bg-green-400 rounded-xl py-3 w-full">Log In</button>
+                    <button className="auth-form-btn">Log In</button>
                 </div>
             </div>
             <div className="flex flex-row gap-6">
-                <Link to="/login" className="text-blue-700 no-underline hover:underline">
+                <Link to="/login" className="auth-form-link">
                     Forgot password?
                 </Link>
-                <Link to="/register" className="text-blue-700 no-underline hover:underline">
+                <Link to="/register" className="auth-form-link">
                     Need an account
                 </Link>
             </div>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,35 +3,35 @@ import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div className="bg-white w-1/4 shadow-xl px-8 py-8 rounded-xl flex flex-col gap-16 items-center justify-start">
+        <div className="auth-form-container">
             <div>
                 {/* 
                 - Logo
                 - Spiel
             */}
-                <h1 className="font-bold text-2xl text-gray-600">Sign Up</h1>
+                <h1 className="auth-form-htext">Sign Up</h1>
             </div>
             <div className="flex flex-col items-center gap-10 w-full">
                 <div className="flex flex-row gap-5 w-full">
                     <div>
-                        <input className="text-xl text-gray-800 border-b-2 border-gray-200 outline-none w-full" placeholder="First name" />
+                        <input className="auth-form-input" placeholder="First name" />
                     </div>
                     <div>
-                        <input className="text-xl text-gray-800 border-b-2 border-gray-200 outline-none w-full" placeholder="Last name" />
+                        <input className="auth-form-input" placeholder="Last name" />
                     </div>
                 </div>
                 <div className="w-full">
-                    <input className="text-xl text-gray-800 border-b-2 border-gray-200 outline-none w-full" placeholder="Email" />
+                    <input className="auth-form-input" placeholder="Email" />
                 </div>
                 <div className="w-full">
-                    <input className="text-xl text-gray-800 border-b-2 border-gray-200 outline-none w-full" type="password" placeholder="Password" />
+                    <input className="auth-form-input" type="password" placeholder="Password" />
                 </div>
                 <div className="w-full">
-                    <button className="font-bold text-white text-xl bg-green-400 rounded-xl py-3 w-full">Sign Up</button>
+                    <button className="auth-form-btn">Sign Up</button>
                 </div>
             </div>
             <div>
-                <Link to="/login" className="text-blue-700 no-underline hover:underline">
+                <Link to="/login" className="auth-form-link">
                     Already have an account?
                 </Link>
             </div>
