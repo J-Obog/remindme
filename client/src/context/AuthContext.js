43f215c161c77fb366fc30822109052599e48dbb
JObog <jobogbaimhe@gmail.com>
2022-02-23 14:45:07 -0500

    defined login and logout functions in auth provider

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,13 +1,31 @@
-import { React, createContext } from 'react';
+import { React, createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-    const login = () => {};
-    const register = () => {};
-    const logout = () => {};
+    const [auth, setAuth] = useState({
+        accessToken: localStorage.getItem(process.env.REACT_APP_ACCESSTK_KEY),
+        refreshToken: localStorage.getItem(process.env.REACT_APP_REFRESHTK_KEY),
+        authenticated: false,
+    });
 
-    return <AuthContext.Provider value={{ login, logout, register }}>{children}</AuthContext.Provider>;
+    useEffect(() => {
+        if (!auth.accessToken || !auth.refreshToken) return;
+    }, [auth, setAuth]);
+
+    const login = (accessToken, refreshToken) => {
+        localStorage.setItem(process.env.REACT_APP_ACCESSTK_KEY, accessToken);
+        localStorage.setItem(process.env.REACT_APP_REFRESHTK_KEY, refreshToken);
+        setAuth({ ...auth, authenticated: true, accessToken, refreshToken });
+    };
+
+    const logout = () => {
+        localStorage.setItem(process.env.REACT_APP_ACCESSTK_KEY, null);
+        localStorage.setItem(process.env.REACT_APP_REFRESHTK_KEY, null);
+        setAuth({ authenticated: true, accessToken: null, refreshToken: null });
+    };
+
+    return <AuthContext.Provider value={{ auth, login, logout }}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
